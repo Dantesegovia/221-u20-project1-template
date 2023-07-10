1798a268b715deec35b1321fd4f78ef6558f6de4
Dante Segovia <ds0482@students.necsd.net>
2023-07-10 12:01:20 -0400

    hello

---
+++ client/public/js/feed.js
@@ -0,0 +1,37 @@
+document.getElementById('portal_button').addEventListener('click', event => {
+    goToLocation('https://my.marist.edu/'); 
+})
+
+let feedItem = {
+    Title: 'News from Marist',
+    Body: 'Get the latest Marist news here',
+    linkUrl: 'hhtps://www.maristcircle.com', 
+    imageUrl: '/images/news_pic.jpg'
+}
+
+let feedItem2 = {
+    Title: 'Dante Segovia is boxing Greg Mayer',
+    Body: 'On July 15th, 2023, Greg Mayer is boxing Dante Segovia at Gregs grandmothers house.',
+    linkUrl: 'https://en.wikipedia.org/wiki/Boxing', 
+    imageUrl: '/images/me.jpeg'
+}
+
+let feedItem3 = {
+    Title: 'Newburgh offers free Marist program',
+    Body: 'NECSD is offering students a chance to get one full year free from college.',
+    linkUrl: 'https://www.newburghschools.org/', 
+    imageUrl: '/images/you.jpeg'
+}
+
+var currentStories = [feedItem, feedItem2, feedItem3]
+
+window.addEventListener("load", () => {
+    for(var i=0; i < currentStories.length; i++){
+        displayFeed(currentStories[i]);
+}})
+
+function displayFeed(currentStories){
+    let feed = document.getElementById
+
+
+}
